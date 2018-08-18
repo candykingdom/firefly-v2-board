fd862841fb27ebc386c4b6f995c1f6adb67d73c4
Adam Demuri <adam.demuri@gmail.com>
2018-08-18 13:18:48 -0600

    git subrepo clone https://ademuri@code.andrewzallen.com/hardware/firefly2 libraries/firefly2
    subrepo:
      subdir:   "libraries/firefly2"
      merged:   "3c64043"
    upstream:
      origin:   "https://ademuri@code.andrewzallen.com/hardware/firefly2"
      branch:   "master"
      commit:   "3c64043"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ libraries/firefly2/software/generic/test/RadioStateMachineTest.cpp
@@ -0,0 +1,148 @@
+#include "gtest/gtest.h"
+
+#include "../NetworkManager.hpp"
+#include "../Radio.hpp"
+#include "../RadioStateMachine.hpp"
+#include "FakeRadio.hpp"
+
+const uint16_t kMaxSlaveTimeout = RadioStateMachine::kSlaveNoPacketTimeout +
+                                  RadioStateMachine::kSlaveNoPacketRandom + 1;
+
+TEST(RadioStateMachine, initializes) {
+  setMillis(0);
+  FakeRadio radio;
+  NetworkManager *networkManager = new NetworkManager(&radio);
+  RadioStateMachine *stateMachine = new RadioStateMachine(networkManager);
+
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+}
+
+TEST(RadioStateMachine, becomesMasterAfterTimeout) {
+  setMillis(0);
+  FakeRadio radio;
+  NetworkManager *networkManager = new NetworkManager(&radio);
+  RadioStateMachine *stateMachine = new RadioStateMachine(networkManager);
+
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+
+  setMillis(kMaxSlaveTimeout);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Master);
+}
+
+TEST(RadioStateMachine, sendsHeartbeats) {
+  setMillis(0);
+  FakeRadio radio;
+  NetworkManager *networkManager = new NetworkManager(&radio);
+  RadioStateMachine *stateMachine = new RadioStateMachine(networkManager);
+
+  setMillis(kMaxSlaveTimeout);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Master);
+  RadioPacket *packet = radio.getSentPacket();
+  ASSERT_NE(packet, nullptr);
+  EXPECT_EQ(packet->type, HEARTBEAT);
+
+  advanceMillis(RadioStateMachine::kMasterHeartbeatInterval + 1);
+  stateMachine->Tick();
+  packet = radio.getSentPacket();
+  ASSERT_NE(packet, nullptr);
+  EXPECT_EQ(packet->type, HEARTBEAT);
+
+  advanceMillis(RadioStateMachine::kMasterHeartbeatInterval);
+  stateMachine->Tick();
+  packet = radio.getSentPacket();
+  ASSERT_NE(packet, nullptr);
+  EXPECT_EQ(packet->type, HEARTBEAT);
+}
+
+TEST(RadioStateMachine, doesntBecomeMasterIfReceivesPackets) {
+  setMillis(0);
+  FakeRadio radio;
+  NetworkManager *networkManager = new NetworkManager(&radio);
+  RadioStateMachine *stateMachine = new RadioStateMachine(networkManager);
+  RadioPacket packet;
+  packet.packetId = 1;
+  packet.type = HEARTBEAT;
+  packet.dataLength = 0;
+
+  radio.setReceivedPacket(&packet);
+  advanceMillis(kMaxSlaveTimeout);
+  advanceMillis(2);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+
+  advanceMillis(RadioStateMachine::kMasterHeartbeatInterval);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+
+  advanceMillis(RadioStateMachine::kMasterHeartbeatInterval);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+}
+
+TEST(RadioStateMachine, respectsClaimMaster) {
+  setMillis(0);
+  FakeRadio radio;
+  NetworkManager *networkManager = new NetworkManager(&radio);
+  RadioStateMachine *stateMachine = new RadioStateMachine(networkManager);
+
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+  setMillis(kMaxSlaveTimeout);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Master);
+
+  RadioPacket packet;
+  packet.packetId = 1;
+  packet.type = CLAIM_MASTER;
+  packet.dataLength = 0;
+  radio.setReceivedPacket(&packet);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+}
+
+TEST(RadioStateMachine, doesElectionAndBecomesSlave) {
+  setMillis(0);
+  FakeRadio radio;
+  NetworkManager *networkManager = new NetworkManager(&radio);
+  RadioStateMachine *stateMachine = new RadioStateMachine(networkManager);
+
+  setMillis(kMaxSlaveTimeout);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Master);
+
+  RadioPacket packet;
+  // The random ID for master election is in the range [2, 0xFFFF)
+  packet.packetId = 0xFFFF;
+  packet.type = HEARTBEAT;
+  packet.dataLength = 0;
+  radio.setReceivedPacket(&packet);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Slave);
+}
+
+TEST(RadioStateMachine, doesElectionAndBecomesMaster) {
+  setMillis(0);
+  FakeRadio radio;
+  NetworkManager *networkManager = new NetworkManager(&radio);
+  RadioStateMachine *stateMachine = new RadioStateMachine(networkManager);
+
+  setMillis(kMaxSlaveTimeout);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Master);
+
+  RadioPacket packet;
+  // The random ID for master election is in the range [2, 0xFFFF)
+  packet.packetId = 1;
+  packet.type = HEARTBEAT;
+  packet.dataLength = 0;
+  radio.setReceivedPacket(&packet);
+  stateMachine->Tick();
+  EXPECT_EQ(stateMachine->GetCurrentState(), RadioState::Master);
+
+  RadioPacket *receivedPacket = radio.getSentPacket();
+  EXPECT_NE(receivedPacket, nullptr);
+  EXPECT_EQ(receivedPacket->type, CLAIM_MASTER);
+}
