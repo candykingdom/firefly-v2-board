d1376a978abc5a1c06eefaf245a74397aa873842
Adam Demuri <adam.demuri@gmail.com>
2019-02-03 20:50:16 -0700

    Update bossa archive to have a root folder
    Also fix the platform.txt to specify the new path for bossa. As of this
    commit, bossa work correctly.

--- docs/tools/update.sh
+++ docs/tools/update.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
 
-tar -cvjSf bossac_linux.tar.bz2 bossac_linux
+set -euo pipefail
+
+DIR=bossac-1.7.0
+
+mkdir $DIR
+cp bossac_linux $DIR
+tar -cvjSf bossac_linux.tar.bz2 $DIR
+rm $DIR/bossac_linux
+rmdir $DIR
 
