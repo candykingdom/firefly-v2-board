bd2152dbaf97f55fb3201f2ef31ab84d9d0ab8bf
Adam Demuri <adam.demuri@gmail.com>
2019-02-03 18:01:50 -0700

    Remove firefly2 subrepo - step 1 of moving out of libraries

--- libraries/firefly2/ci.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd software/generic
-mkdir build || true
-cd build
-cmake ..
-make && ./generictest
--- libraries/firefly2/software/generic/test/DebugTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-
-#include "../Debug.hpp"
-
-TEST(Debug, shouldNotBeEnabled) {
-#if DEBUG
-  FAIL() << "You accidentally left debug mode on in Debug.hpp. Turn it off";
-#endif
-}
